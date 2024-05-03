af0bc0a9a2dbcd32623f4e3152d73947ab4df6aa
Alexander King Perocho <perocho.xela@gmail.com>
2024-05-02 22:31:25 -0400

    playground styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import NomadLandingTitle from 'components/NomadLandingTitle';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World!</h1>
+    <main>
+      <NomadLandingTitle />
     </main>
   );
 }
