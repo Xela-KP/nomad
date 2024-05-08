c4fd13c887dc81a947c7d90acbd953b37bb51118
Alexander King Perocho <perocho.xela@gmail.com>
2024-05-08 09:15:24 -0400

    added boiler for about

--- src/components/content/About.tsx
+++ src/components/content/About.tsx
@@ -1,10 +1,14 @@
 const About = () => (
-  <div>
-    <h1 className="text-7xl">Who are we?</h1>
-    <p className="max-w-md">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id nisi sed elit sodales
-      fermentum vel tincidunt sapien. Aenean sed mauris eget sem mattis maximus a non mi.
-    </p>
-  </div>
+  <section id="about" className="bg-black text-beige min-h-screen p-16">
+    <div className="relative flex flex-row justify-center align-middle flex-grow rounded-2xl">
+      <div className="text-center space-y-4">
+        <h1 className="text-7xl">Who are we?</h1>
+        <p className="max-w-3xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id nisi sed elit sodales
+          fermentum vel tincidunt sapien. Aenean sed mauris eget sem mattis maximus a non mi.
+        </p>
+      </div>
+    </div>
+  </section>
 );
 export default About;
